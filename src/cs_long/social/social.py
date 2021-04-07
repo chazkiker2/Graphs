23e0acab312f097879c24d1e9a11985035c48f79
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-07 18:19:06 -0400

    implement bi-directional search for faster shortest-path

--- src/cs_long/social/social.py
+++ src/cs_long/social/social.py
@@ -14,6 +14,8 @@ class SocialGraph:
         self.friendships = {}
 
     def add_friendships(self, user_id, *friends):
+        """creates friendships between the user with the given ID and an iterable of all the other friends"""
+
         for friend in friends:
             self.add_friendship(user_id, friend)
 
@@ -38,6 +40,7 @@ class SocialGraph:
 
     def populate_graph(self, num_users, avg_friendships):
         """
+        Populates
         Takes a number of users and an average number of friendships
         as arguments
         Creates that number of users and a randomly distributed friendships
@@ -61,7 +64,16 @@ class SocialGraph:
             f1, f2 = potential_friendships[i]
             self.add_friendship(f1, f2)
 
-    def get_shortest_social_path(self, start, end):
+    # BFS breadth-first search implementation
+    # @see get_shortest_social_path for bi-directional (faster) implementation
+    def get_shortest_bfs(self, start, destination):
+        """
+        returns the shortest path between the starting friend and destination friend
+
+        !BFS implementation!
+        @see get_shortest_social_path for bi-directional (faster) implementation
+        """
+
         q = deque()
         q.append([start])
         visited = set()
@@ -69,19 +81,98 @@ class SocialGraph:
             path = q.popleft()
             friend = path[-1]
             if friend not in visited:
-                if friend == end:
+                if friend == destination:
                     return path
                 visited.add(friend)
                 for next_friend in self.friendships[friend]:
                     q.append(path.copy() + [next_friend])
 
+    # bi-directional search implementation
+    def get_shortest_social_path(self, start, destination):
+        """
+        returns the shortest path between the starting friend and destination friend
+
+        !BI-DIRECTIONAL SEARCH IMPLEMENTATION!
+        @see get_shortest_bfs for BFS (slower) implementation
+
+        Bi-Directional Search operates by running two BFS simultaneously,
+        one from the starting node and one from the destination node
+        Each iteration, we'll take one extend the two paths by one more level
+        Once the paths overlap, we know we've hit a potential solution
+        """
+
+        # if they are the same user
+        if start == destination:
+            # then there is only one shortest path to self
+            return [start]
+
+        # dictionary of currently active vertices with their corresponding paths.
+        active_paths = {
+            start: [start],  # path from start to _some_node_
+            destination: [destination]  # path from destination node to _some_node_
+        }
+
+        # will contain vertices that we've ruled out along the way
+        inactive_verts = set()
+
+        # while there are still potential solution paths
+        while active_paths:
+            # every key in active_paths is an active vertex
+            active_verts = list(active_paths.keys())
+
+            # for every active vertex
+            for vert in active_verts:
+                # the current path from node to somewhere
+                current_path = active_paths[vert]
+                # starting node in this path
+                origin = current_path[0]
+                # all unique friends that are still active
+                current_neighbors = set(self.friendships[vert]) - inactive_verts
+
+                # check if our current neighbors hit an active vertex
+                # this would happen once each BFS overlaps
+                # if this happens, we have a potential solution path
+                for meeting_vert in current_neighbors.intersection(active_verts):
+                    # check to make sure that the two paths didn't start at the same place
+                    # if they didn't, then we've got a path from start to destination
+                    if origin != active_paths[meeting_vert][0]:
+                        # reverse one of the paths
+                        active_paths[meeting_vert].reverse()
+                        # combine the results
+                        joined_paths = active_paths[vert] + active_paths[meeting_vert]
+                        # check if joined_paths starts with the proper starting friend
+                        if joined_paths[0] != start:
+                            # if joined paths is in reverse order (i.e, from destination friend to starting friend)
+                            # then we'll reverse it to start at the starting friend
+                            joined_paths.reverse()
+                        # return our joined paths from start to destination
+                        return joined_paths
+
+                # if we're here, we did not find a solution at this level
+
+                # so check for new neighbours to extend our paths another level
+                new_neighbors = set(current_neighbors) - inactive_verts - set(active_verts)
+                # for every new neighbor
+                # extend the paths, remove the previous one, and update the active vertices
+                for neighbor_vert in new_neighbors:
+                    # extend path and update record
+                    active_paths[neighbor_vert] = current_path + [neighbor_vert]
+                    # add the neighbor vert to our list of active vertices
+                    active_verts.append(neighbor_vert)
+
+                # finally, remove the current path (b/c we did not get a solution)
+                # and record the current vert as an inactive endpoint.
+                active_paths.pop(vert, None)
+                inactive_verts.add(vert)
+
     def get_all_social_paths(self, user_id):
         """
-        :param user_id: the relevant user for which to find an extended network
+        find a detailed extended network for the user with the given id
 
-        Returns a dictionary containing every user in that user's extended network
-        with the shortest friendship path between them.
-        The key is the relevant friend's ID; the value is the shortest path between them
+        Returns a dictionary containing every user in that user's extended network with the
+        shortest friendship path between them. The key is the relevant friend's ID; the
+        value is the shortest path between them
+        :param user_id: the relevant user for which to find an extended network
         """
         stack = deque()
         stack.append(user_id)
