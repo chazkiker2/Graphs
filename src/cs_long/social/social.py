4ceb9c3c1fc2e73156069970c854c73ae49ece86
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-08 13:23:13 -0400

    add alternative methods from lecture

--- src/cs_long/social/social.py
+++ src/cs_long/social/social.py
@@ -1,5 +1,22 @@
 from collections import deque
 import random
+import time
+
+
+class Queue:
+    def __init__(self):
+        self.storage = deque()
+
+    def __len__(self):
+        return len(self.storage)
+
+    def enqueue(self, value):
+        # add to right (tail)
+        self.storage.append(value)
+
+    def dequeue(self):
+        # take from left (head)
+        return self.storage.popleft()
 
 
 class User:
@@ -23,12 +40,15 @@ class SocialGraph:
         """Creates a bi-directional friendship"""
 
         if user_id == friend_id:
-            print("WARNING: You cannot be friends with yourself")
+            # print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
-            print("WARNING: Friendship already exists")
+            # print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -38,19 +58,14 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
-    def populate_graph(self, num_users, avg_friendships):
-        """
-        Populates
-        Takes a number of users and an average number of friendships
-        as arguments
-        Creates that number of users and a randomly distributed friendships
-        between those users.
-        The number of users must be greater than the average number of friendships.
-        """
-        # Reset graph
+    def reset_network(self):
         self.last_id = 0
         self.users = {}
         self.friendships = {}
+
+    def populate_graph(self, num_users, avg_friendships):
+        self.reset_network()
+
         for i in range(num_users):
             self.add_user(f"test_user_{i}")
 
@@ -63,6 +78,38 @@ class SocialGraph:
         for i in range((num_users * avg_friendships) // 2):
             f1, f2 = potential_friendships[i]
             self.add_friendship(f1, f2)
+
+    def populate_graph_linear(self, num_users, avg_friendships):
+        self.reset_network()
+
+        for i in range(num_users):
+            self.add_user(f"test_user_{i}")
+
+        # set a target to friendships to (num_users * avg_friendships)
+        target_friendships = num_users * avg_friendships
+        # create a counter for the total number of friendships created and set it to zero
+        current_friendship_count = 0
+        # create a collisions counter and set it to zero
+        collisions = 0
+
+        # iterate while the target friendships are less than total friendships
+        while current_friendship_count < target_friendships:
+
+            # randomly pick a user as the user_id
+            user_id = random.randint(1, self.last_id)
+            # randomly pick a user as the friend_id
+            friend_id = random.randint(1, self.last_id)
+
+            # if an add_friendship call returns true –– then we added 2 friendships
+            res = self.add_friendship(user_id, friend_id)
+            if res:
+                # add 2 to the total friendships (due to bi-directional nature of connections)
+                current_friendship_count += 2
+            else:
+                # increment collisions
+                collisions += 1
+
+        print(f"collisions={collisions}")
 
     # BFS breadth-first search implementation
     # @see get_shortest_social_path for bi-directional (faster) implementation
@@ -95,9 +142,10 @@ class SocialGraph:
         !BI-DIRECTIONAL SEARCH IMPLEMENTATION!
         @see get_shortest_bfs for BFS (slower) implementation
 
-        Bi-Directional Search operates by running two BFS simultaneously,
-        one from the starting node and one from the destination node
-        Each iteration, we'll take one extend the two paths by one more level
+        Bi-Directional Search operates by running two BFS simultaneously:
+        - one from the starting node
+        - one from the destination node
+        Each iteration, we'll take extend the two paths by one more level
         Once the paths overlap, we know we've hit a potential solution
         """
 
@@ -174,26 +222,68 @@ class SocialGraph:
         value is the shortest path between them
         :param user_id: the relevant user for which to find an extended network
         """
+
+        # start with a DFS to get the extended social network
+        # using deque in LIFO fashion: add to tail, take from tail
         stack = deque()
+        # add user_id to the top of the stack
         stack.append(user_id)
+        # will keep track of each friend in extended network
         friend_paths = {}
 
+        # while stack is not empty
         while stack:
+            # take our current friend off the top of the stack
             friend = stack.pop()
+            # if we haven't yet visited this friend
             if friend not in friend_paths:
+                # make an empty entry in our friend_paths
                 friend_paths[friend] = None
-                for next_friend in self.friendships[friend]:
-                    stack.append(next_friend)
+                # for every connection that this friend has
+                for connection in self.friendships[friend]:
+                    # add connection to the top of our stack
+                    stack.append(connection)
 
+        # currently, my get_shortest_social_path employs a Bi-Directional Search
+        # to find the shortest path between a Source and Destination.
+        # This could also be a BFS, but the Bi-Directional is wildly more scalable
         return {
+            # get the shortest path between the input user_id and
+            # each friend_id in the network of extended friends
             friend_id: self.get_shortest_social_path(user_id, friend_id)
             for friend_id in friend_paths
         }
 
+    def get_all_social_paths_lecture(self, user_id):
+        q = deque()
+        q.append([user_id])
+        visited = {}
+
+        while q:
+            current_path = q.popleft()
+            current_user_id = current_path[-1]
+            if current_user_id not in visited:
+                visited[current_user_id] = current_path
+                for friend in self.friendships[current_user_id]:
+                    q.append(current_path + [friend])
+
+        return visited
+
+    def get_all_social_paths_linear(self, user_id):
+        pass
+
 
 if __name__ == '__main__':
+    num_users = 10000
+    avg_friendships = 15000
+
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
-    print(f"sg.friendships={sg.friendships}")
-    connections = sg.get_all_social_paths(1)
-    print(f"connections={connections}")
+    start_time = time.time()
+    sg.populate_graph(num_users, avg_friendships)
+    end_time = time.time()
+    print(f"QUADRATIC --- {end_time - start_time} seconds")
+
+    start_time2 = time.time()
+    sg.populate_graph_linear(num_users, avg_friendships)
+    end_time2 = time.time()
+    print(f"LINEAR --- {end_time2 - start_time2} seconds")
